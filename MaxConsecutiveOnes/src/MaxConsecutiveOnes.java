625d13d10fc5765afc4a2e3cd1aba9d5bfd92a21
Lam Nguyen <lqn2257@truman.edu>
2017-08-24 18:56:20 -0500

    add Consecutive Ones prob

---
+++ MaxConsecutiveOnes/src/MaxConsecutiveOnes.java
@@ -0,0 +1,32 @@
+/*
+ * Given a binary array, find the maximum number of consecutive 1s in this array.
+ * 
+ * Note:
+ * The input array will only contain 0 and 1.
+ * The length of input array is a positive integer and will not exceed 10,000
+ * 
+ */
+
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+    	int index = 0;
+    	int maxConsecutive = 0;
+    	int consecutive = 0;
+    	
+    	while (index < nums.length) {
+    		if (nums[index] == 1) {
+    			consecutive++;
+    		}
+    		else {
+    			consecutive = 0;
+    		}
+    		if (maxConsecutive < consecutive) {
+    			maxConsecutive = consecutive;
+    		}
+    		index++;
+    	}
+        
+    	return maxConsecutive;
+    }
+
+}
