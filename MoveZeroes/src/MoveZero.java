e21ab578fec3af4877f4911692641206897630da
Lam Nguyen <lqn2257@truman.edu>
2017-08-30 12:46:45 -0500

    add Move Zeroes prob

---
+++ MoveZeroes/src/MoveZero.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+/*
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+ * Note:
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ * 
+ */
+
+
+public class MoveZero {
+	
+    public void moveZeroes(int[] nums) {
+        int zeroSlot = nums.length - 1;
+        int zeroFinder = 0;
+        
+        for (int i = nums.length - 1; i >= 0; i--) {
+        	if (nums[i] == 0) {
+        		zeroFinder = i;
+        		while (zeroFinder != zeroSlot) {
+//        			System.out.println(zeroFinder + " " + zeroSlot);
+        			if (nums[zeroFinder + 1] != 0) {
+        				//swap
+        				nums[zeroFinder] = nums[zeroFinder + 1];
+        				nums[zeroFinder + 1] = 0;
+        			}
+        			zeroFinder++;
+        		}
+        		zeroSlot--;
+        	}
+        }
+//        System.out.println(Arrays.toString(nums));
+    }
+	
+	
+	//Copy the input array
+    public void moveZeroesCopy(int[] nums) {
+    	int[] result = new int[nums.length];
+    	int resultIndexTail = result.length - 1;
+    	int resultIndexHead = 0;
+    	
+    	for (int i = 0; i < nums.length; i++) {
+//    		System.out.println(nums[i]);
+    		if (nums[i] != 0) {
+    			result[resultIndexHead] = nums[i];
+    			resultIndexHead++;
+    		}
+    		else {
+    			result[resultIndexTail] = nums[i];
+    			resultIndexTail--;
+    		}
+    	}
+    	
+//    	System.out.println(Arrays.toString(nums));
+//    	System.out.println(Arrays.toString(result));
+        nums = result;
+    }
+
+}
---
+++ MoveZeroes/src/Tester.java
@@ -0,0 +1,10 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		int[] test = {0, 1, 0, 3, 12};
+		MoveZero ob = new MoveZero();
+		ob.moveZeroes(test);
+	}
+
+}
