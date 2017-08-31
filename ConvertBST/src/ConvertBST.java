6d8b855450fbca8cf22de9f9a203e71709f26b1e
Lam Nguyen <lqn2257@truman.edu>
2017-08-31 09:17:09 -0500

    add description

--- ConvertBST/src/ConvertBST.java
+++ ConvertBST/src/ConvertBST.java
@@ -1,3 +1,9 @@
+/*
+ * Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to 
+ * the original key plus sum of all keys greater than the original key in BST.
+ * 
+ */
+
 
 public class ConvertBST {
 	private int sum = 0;
