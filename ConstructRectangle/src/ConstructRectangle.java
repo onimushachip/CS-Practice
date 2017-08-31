ce31dcdd9b0926ef86833b5d5c98ccfeee53ba6e
Lam Nguyen <lqn2257@truman.edu>
2017-08-31 11:12:20 -0500

    add construct rectangle prob

---
+++ ConstructRectangle/src/ConstructRectangle.java
@@ -0,0 +1,30 @@
+/*
+ * For a web developer, it is very important to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+ * 1. The area of the rectangular web page you designed must equal to the given target area.
+ * 2. The width W should not be larger than the length L, which means L >= W.
+ * 3. The difference between length L and width W should be as small as possible.
+ * 
+ */
+public class ConstructRectangle {
+    public int[] constructRectangle(int area) {
+    	int length = area;
+    	int width = 0;
+    	int[] result = new int[2];
+    	
+    	while (true) {
+    		if (area % length == 0) {
+    			width = area / length;
+    			if (width <= length) {
+    				result[0] = length;
+        			result[1] = width;
+    			}
+    			else {
+    				break;
+    			}
+    		}
+    		length--;
+    	}
+        return result;
+    }
+
+}
---
+++ ConstructRectangle/src/Tester.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+
+public class Tester {
+
+	public static void main(String[] args) {
+		ConstructRectangle ob = new ConstructRectangle();
+		int[] result = ob.constructRectangle(256);
+		System.out.println(Arrays.toString(result));
+	}
+
+}
