d0626abc01880c3023648f84e50d880b000ce85d
Lam Nguyen <lqn2257@truman.edu>
2017-09-01 00:36:01 -0500

    add Equal Array prob

---
+++ MinMoveToEqualArray/src/EqualArray.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+/*
+ * Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, 
+ * where a move is incrementing n - 1 elements by 1.
+ * 
+ */
+
+
+public class EqualArray {
+	public int minMoves(int[] nums) {
+		if (nums.length == 1) {
+			return 0;
+		}
+		Arrays.sort(nums);
+		int steps = 0;
+		for (int i = nums.length - 1; i > 0; i--) {
+			steps += nums[i] - nums[0];
+		}
+		return steps;
+	}
+	
+	
+	//Experimental method
+    public int minMovesSum(int[] nums) {
+    	int steps = 0;
+    	int sum = 0;
+    	int maxNumber = 0;
+    	
+    	for (int i = 0; i < nums.length; i++) {
+    		if (maxNumber < nums[i]) {
+    			maxNumber = nums[i];
+    		}
+    		sum += nums[i];
+    	}
+    	
+    	steps = sum % nums.length;
+    	
+    	if (steps == 0 && sum < maxNumber * nums.length) {
+    		return nums.length;
+    	}
+    	else {
+    		return steps; 
+    	}     
+    }
+}
---
+++ MinMoveToEqualArray/src/Tester.java
@@ -0,0 +1,10 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		double test = 2147483647 + 1;
+		System.out.println(test % 2);
+
+	}
+
+}
