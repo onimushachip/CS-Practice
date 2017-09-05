953b74a20985d8406fd43cd4413c12baa50cc323
Lam Nguyen <lqn2257@truman.edu>
2017-09-05 12:10:52 -0500

    add BST system and in-order traversal

--- MinimumAbsoluteDifferenceBST/src/Tester.java
+++ MinimumAbsoluteDifferenceBST/src/Tester.java
@@ -2,15 +2,37 @@
 public class Tester {
 
 	public static void main(String[] args) {
-		TreeNode node1 = new TreeNode(1);
+//		TreeNode node1 = new TreeNode(1);
+//		TreeNode node3 = new TreeNode(3);
+//		TreeNode node2 = new TreeNode(2);
+//		
+//		node1.right = node3;
+//		node3.left = node2;
+//		
+//		MinimumDifferenceBST ob = new MinimumDifferenceBST();
+//		System.out.println(ob.getMinimumDifference(node1));
+		
+		TreeNode node8 = new TreeNode(8);
 		TreeNode node3 = new TreeNode(3);
-		TreeNode node2 = new TreeNode(2);
+		TreeNode node10 = new TreeNode(10);
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node6 = new TreeNode(6);
+		TreeNode node14 = new TreeNode(14);
+		TreeNode node4 = new TreeNode(4);
+		TreeNode node7 = new TreeNode(7);
+		TreeNode node13 = new TreeNode(13);
 		
-		node1.right = node3;
-		node3.left = node2;
+		TreeManage ob = new TreeManage();
+		ob.addNode(node8, node3);
+		ob.addNode(node8, node10);
+		ob.addNode(node8, node1);
+		ob.addNode(node8, node6);
+		ob.addNode(node8, node14);
+		ob.addNode(node8, node4);
+		ob.addNode(node8, node7);
+		ob.addNode(node8, node13);
 		
-		MinimumDifferenceBST ob = new MinimumDifferenceBST();
-		System.out.println(ob.getMinimumDifference(node1));
+		ob.printTree(node8);
 
 	}
 
---
+++ MinimumAbsoluteDifferenceBST/src/TreeManage.java
@@ -0,0 +1,47 @@
+
+public class TreeManage {
+	public void addNode(TreeNode tree, TreeNode newNode) {
+		if (tree == null || newNode == null) {
+			return;
+		}
+		
+		//Left
+		if (tree.val > newNode.val) {
+			if (tree.left == null) {
+				tree.left = newNode;
+			}
+			else {
+				addNode(tree.left, newNode);
+			}
+		}
+		
+		//Right
+		if (tree.val < newNode.val) {
+			if (tree.right == null) {
+				tree.right = newNode;
+			}
+			else {
+				addNode(tree.right, newNode);
+			}
+		}
+		System.out.println("check");
+	}
+	
+	//In-order traverse
+	public void printTree(TreeNode tree) {
+		if (tree == null) {
+			return;
+		}
+		
+		if (tree.left != null) {
+			printTree(tree.left);
+		}
+		
+		System.out.println(tree.val);
+		
+		if (tree.right != null) {
+			printTree(tree.right);
+		}
+	}
+
+}
