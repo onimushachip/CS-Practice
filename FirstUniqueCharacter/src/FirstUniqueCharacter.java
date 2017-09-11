6e4f741488371020680db7d0718dc60544164c4d
Lam Nguyen <lqn2257@truman.edu>
2017-09-11 18:23:13 -0500

    add First Unique Character prob

---
+++ FirstUniqueCharacter/src/FirstUniqueCharacter.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+ * 
+ */
+public class FirstUniqueCharacter {
+    public int firstUniqChar(String s) {
+//    	boolean unique = false;
+    	Set<Integer> apprearedChar = new HashSet<Integer>();
+    	Set<Integer> repeatedChar = new HashSet<Integer>();
+    	
+    	if (s.length() == 1) {
+    		return 0;
+    	}
+       	
+//    	candidate = s.codePointAt(0);
+    	for (int i = 0; i < s.length(); i++) {
+    		if (!apprearedChar.contains(s.codePointAt(i))) {
+    			apprearedChar.add(s.codePointAt(i));
+    		}
+    		else {
+    			repeatedChar.add(s.codePointAt(i));
+    		}
+    	}
+    	
+    	for (int i = 0; i < s.length(); i++) {
+    		System.out.println(s.codePointAt(i));
+    		if (!repeatedChar.contains(s.codePointAt(i))) {
+    			return i;
+    		}
+    	}
+    	
+    	return -1;
+    }
+
+}
---
+++ FirstUniqueCharacter/src/Tester.java
@@ -0,0 +1,13 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		FirstUniqueCharacter ob = new FirstUniqueCharacter();
+		String test = "loveleetcode";
+		int result = ob.firstUniqChar(test);
+		
+		System.out.println(result);
+
+	}
+
+}
