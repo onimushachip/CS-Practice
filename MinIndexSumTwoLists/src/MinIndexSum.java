6a6682dcdacc1c5b8d1b8e1b8f3824b89f4637ef
Lam Nguyen <lqn2257@truman.edu>
2017-09-12 12:46:20 -0500

    add Min Index Sum 2 Lists prob

---
+++ MinIndexSumTwoLists/src/MinIndexSum.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+ * Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
+ * You need to help them find out their common interest with the least list index sum. 
+ * If there is a choice tie between answers, output all of them with no order requirement. 
+ * You could assume there always exists an answer.
+ * 
+ */
+public class MinIndexSum {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+    	String[] result = null;
+    	int minSum = Integer.MAX_VALUE;
+    	Queue<String> resultQueue = new LinkedList<String>();
+    	HashMap<String, Integer> indexList = new HashMap<String, Integer>();
+    	
+    	for (int i = 0; i < list1.length; i++) {
+    		indexList.put(list1[i], i);
+    	}
+    	
+    	for (int i = 0; i < list2.length; i++) {
+    		if (indexList.containsKey(list2[i])) {
+    			if ((i + indexList.get(list2[i]) < minSum)) {
+    				minSum = i + indexList.get(list2[i]);
+    			}
+    		}
+    	}
+    	
+    	for (int i = 0; i < list2.length; i++) {
+    		if (indexList.containsKey(list2[i])) {
+    			if ((i + indexList.get(list2[i]) == minSum)) {
+    				resultQueue.add(list2[i]);
+    				System.out.println(list2[i]);
+    			}
+    		}
+    	}
+    	
+    	result = new String[resultQueue.size()];
+    	System.out.println(Arrays.asList(resultQueue));
+    	
+    	for (int i = 0; i < result.length; i++) {
+//    		System.out.println(resultQueue.peek());
+    		result[i] = resultQueue.poll();
+    	}
+    	
+    	System.out.println(Arrays.toString(result));
+    	
+    	return result;
+    }
+
+}
---
+++ MinIndexSumTwoLists/src/Tester.java
@@ -0,0 +1,13 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		String[] list1 = {"a", "b", "c", "d"};
+		String[] list2 = {"d", "c", "b", "a"};
+		
+		MinIndexSum ob = new MinIndexSum();
+		ob.findRestaurant(list1, list2);
+
+	}
+
+}
