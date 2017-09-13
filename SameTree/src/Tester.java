2515dde9b67dea53b504fbe3243ae727ec6c5236
Lam Nguyen <lqn2257@truman.edu>
2017-09-12 22:12:09 -0500

    complete Relative Rank

--- SameTree/src/Tester.java
+++ SameTree/src/Tester.java
@@ -5,7 +5,7 @@ public class Tester {
 	public static void main(String[] args) {
 		RelativeRank ob = new RelativeRank();
 		String[] result = null;
-		int[] test = {8, 9, 3, 1, 0};
+		int[] test = {1};
 		
 		result = ob.findRelativeRanks(test);
 		System.out.println(Arrays.toString(result));
