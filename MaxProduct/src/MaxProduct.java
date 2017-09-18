794561c218d27413f7cceda95402bcb6940e25dd
Lam Nguyen <lqn2257@truman.edu>
2017-09-18 14:29:06 -0500

    add Max Product

---
+++ MaxProduct/src/MaxProduct.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+/*
+ * Given an integer array, find three numbers whose product is maximum and output the maximum product.
+ * 
+ */
+public class MaxProduct {
+    public int maximumProduct(int[] nums) {
+        int result = 0;
+        
+        if (nums.length <= 3) {
+        	result = 1;
+        	for (int i = 0; i < nums.length; i++) {
+        		result = result * nums[i];
+        	}
+        	return result;
+        }
+        
+        Arrays.sort(nums);
+        
+        result = 1;
+        
+        if (nums[0] < 0 && nums[1] < 0) {
+        	result = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+        	if (result < nums[nums.length - 1] * nums[0] * nums[1]) {
+        		result = nums[nums.length - 1] * nums[0] * nums[1];
+        	}
+        }
+        else {
+        	result = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+        }
+//        for (int i = nums.length - 1; i >= nums.length - 3; i--) {
+//        	result = result * nums[i];
+//        }
+    	
+    	return result;
+    }
+}
---
+++ MaxProduct/src/Tester.java
@@ -0,0 +1,12 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		int[] test = {-1, -2, -3, -4, 60};
+		MaxProduct ob = new MaxProduct();
+		int result = ob.maximumProduct(test);
+		System.out.println(result);
+
+	}
+
+}
