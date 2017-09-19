be28648e41304000694d05057480a1f34c27ea73
Lam Nguyen <lqn2257@truman.edu>
2017-09-19 14:00:24 -0500

    add convert base 7 prob

---
+++ ConvertToBaseSeven/src/ConvertToBase7.java
@@ -0,0 +1,22 @@
+/*
+ * Given an integer, return its base 7 string representation.
+ * 
+ */
+public class ConvertToBase7 {
+    public String convertToBase7(int num) {
+    	if (num < 0) {
+    		return "-" + convertToBase7(-num);
+    	}
+    	if (num < 7) {
+    		return Integer.toString(num);
+    	}
+    	
+    	String remainder = Integer.toString(num % 7);
+    	
+//    	System.out.println(remainder);
+    	String result = convertToBase7(num / 7) + remainder;
+//    	System.out.println(result);
+    	return result;
+    }
+
+}
---
+++ ConvertToBaseSeven/src/Tester.java
@@ -0,0 +1,11 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		ConvertToBase7 ob = new ConvertToBase7();
+		String result = ob.convertToBase7(357);
+		System.out.println(result);
+
+	}
+
+}
