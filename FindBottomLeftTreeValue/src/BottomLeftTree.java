3c6bfafa462ca4a8c919ec85fb8ba6f7d28ca4a6
Lam Nguyen <lqn2257@truman.edu>
2017-09-26 17:03:37 -0500

    add Bottom Left Tree Value prob

---
+++ FindBottomLeftTreeValue/src/BottomLeftTree.java
@@ -0,0 +1,37 @@
+/*
+ * Given a binary tree, find the leftmost value in the last row of the tree.
+ * 
+ */
+public class BottomLeftTree {
+	private int distanceLeft = 0;
+	private int bottomLeftVal;
+	
+    public int findBottomLeftValue(TreeNode root) {
+        int result = 0;
+        
+        bottomLeftVal = root.val;
+        
+        traverseTree(root, false, 0);
+        
+        result = bottomLeftVal;
+        
+        return result;
+    }
+    
+    private void traverseTree(TreeNode node, boolean isLeft, int distance) {
+    	if (node == null) {
+    		return;
+    	}
+    	
+    	if (isLeft == true) {
+    		if (distance > distanceLeft) {
+    			bottomLeftVal = node.val;
+    			distanceLeft = distance;
+    		}
+    	}
+    	
+    	traverseTree(node.left, true, distance + 1);
+    	traverseTree(node.right, false, distance + 1);
+    }
+
+}
---
+++ FindBottomLeftTreeValue/src/Tester.java
@@ -0,0 +1,27 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node2 = new TreeNode(2);
+		TreeNode node3 = new TreeNode(3);
+		TreeNode node4 = new TreeNode(4);
+		TreeNode node5 = new TreeNode(5);
+		TreeNode node6 = new TreeNode(6);
+		TreeNode node7 = new TreeNode(7);
+		
+		node1.left = node2;
+		node1.right = node3;
+		node2.left = node4;
+		node3.left = node5;
+		node3.right = node6;
+		node5.left = node7;
+		
+		BottomLeftTree ob = new BottomLeftTree();
+		
+		int result = ob.findBottomLeftValue(node1);
+		
+		System.out.println(result);
+	}
+
+}
