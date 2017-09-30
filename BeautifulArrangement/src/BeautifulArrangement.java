38d785b584cedc00a90dadb662f91a6251748581
Lam Nguyen <lqn2257@truman.edu>
2017-09-30 16:37:15 -0500

    add Beautiful Arrangement prob

---
+++ BeautifulArrangement/src/BeautifulArrangement.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ * Suppose you have N integers from 1 to N. We define a beautiful arrangement as an array that is constructed by these N numbers successfully if one of the following is true for the ith position (1 <= i <= N) in this array:
+ * The number at the ith position is divisible by i.
+ * i is divisible by the number at the ith position.
+ * Now given N, how many beautiful arrangements can you construct?
+ * 
+ */
+public class BeautifulArrangement {
+	private int result = 0;
+	private ArrayList<Integer> givenNumbers = new ArrayList<Integer>();
+	private int[] arrangement;
+	
+    public int countArrangement(int N) {
+    	for (int i = 1; i <= N; i++) {
+    		givenNumbers.add(i);
+    	}
+    	
+    	this.arrangement = new int[N];
+    	
+    	findArrangement(N, 1);
+        
+    	return result;
+    }
+    
+    private void findArrangement(int layer, int index) {
+    	if (layer == 0) {
+//    		System.out.println(Arrays.toString(arrangement));
+    		return;
+    	}
+    	
+    	int listSize = this.givenNumbers.size();
+    	
+    	
+    	for (int i = 0; i < listSize; i++) {
+//    		System.out.print(givenNumbers.get(i) + " ");
+    		if ((index % givenNumbers.get(i) == 0) || (givenNumbers.get(i) % index == 0)) {
+    			int checkedNumber = givenNumbers.get(i);
+    			
+    			arrangement[index - 1] = checkedNumber;
+    			
+    			givenNumbers.remove(i);
+    			
+    			if (layer == 1) {
+    				this.result++;
+    				System.out.println(Arrays.toString(arrangement) + "check");
+//    				System.out.print("bingo ");
+//    				System.out.print("\n");
+    			}
+    			
+    			findArrangement(layer - 1, index + 1);
+    			
+    			givenNumbers.add(i, checkedNumber);
+    		}
+//    		else {
+////    			System.out.print("\n");
+//    			return;
+//    		}
+    	}
+    }
+}
---
+++ BeautifulArrangement/src/Tester.java
@@ -0,0 +1,14 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		int test = 4;
+		
+		BeautifulArrangement ob = new BeautifulArrangement();
+		
+		test = ob.countArrangement(test);
+		
+		System.out.println("Result: " + test);
+	}
+
+}
