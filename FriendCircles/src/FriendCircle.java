f8362379559819fb8fec4201f3f09029293bea33
Lam Nguyen <lqn2257@truman.edu>
2017-10-18 13:31:10 -0500

    progress

--- FriendCircles/src/FriendCircle.java
+++ FriendCircles/src/FriendCircle.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 /*
  * There are N students in a class. Some of them are friends, while some are not. 
  * Their friendship is transitive in nature. For example, if A is a direct friend of B, and B is a direct friend of C, then A is an indirect friend of C. 
@@ -9,21 +13,101 @@
  */
 public class FriendCircle {
     public int findCircleNum(int[][] M) {
-    	
+    	ArrayList<Set> groupList = new ArrayList<Set>();
 //    	int col = 0;
     	
     	for (int friend = 0; friend < M.length; friend++) {
+    		Set<Integer> friendGroup = new HashSet<Integer>();
+    		boolean isNewGroup = true;
+    		int groupIndex = 0;
     		
-    		for (int col = friend - 1; col >= 0; col--) {
-    			System.out.print(M[friend][col] + " ");
+//    		for (int i = 0; i < groupList.size(); i++) {
+//    			if (groupList.get(i).contains(friend)) {
+//    				friendGroup = groupList.get(i); //Check if the friend already belongs to a group
+//    				
+//    				isNewGroup = false;
+//    				
+//    				groupIndex = i;
+//    			}
+//    		}
+    		
+    		friendGroup.add(friend);
+    		
+    		for (int col = friend - 1; col >= 0; col--) { //Scan to the left
+//    			System.out.print(M[friend][col] + " ");
+    			if (M[friend][col] == 1) {
+    				friendGroup.add(col);
+    				
+//    				for (int i = 0; i < groupList.size(); i++) {
+//    					if (groupList.get(i).contains(col)) {
+//    						isNewGroup = false;
+//    						
+//    						for (Integer newFriend : friendGroup) {
+//    							groupList.get(i).add(newFriend);
+//    						}
+//    						
+//    						friendGroup = groupList.get(i);
+//    						
+//    						groupList.remove(groupIndex);
+//    					}
+//    				}
+    			}
     		}
-    		System.out.print(" + ");
-    		for (int row = friend + 1; row < M.length; row++) {
-    			System.out.print(M[row][friend] + " ");
+//    		System.out.print(" + ");
+    		for (int row = friend + 1; row < M.length; row++) { //Scan below
+//    			System.out.print(M[row][friend] + " ");
+    			if (M[row][friend] == 1) {
+    				friendGroup.add(row);
+    				
+//    				for (int i = 0; i < groupList.size(); i++) {
+//    					if (groupList.get(i).contains(row)) {
+//    						isNewGroup = false;
+//    						
+//    						for (Integer newFriend : friendGroup) {
+//    							groupList.get(i).add(newFriend);
+//    						}
+//    						
+//    						friendGroup = groupList.get(i);
+//    						
+//    						groupList.remove(groupIndex);
+//    					}
+//    				}
+    			}
     		}
-    		System.out.print("\n");
+//    		System.out.print("\n");
+    		
+    		for (Integer newFriend : friendGroup) {
+    			for (int i = 0; i < groupList.size(); i++) {
+    				if (groupList.get(i).contains(newFriend)) {
+    					if (isNewGroup == true) {
+        					isNewGroup = false;
+        					
+        					groupList.get(i).addAll(friendGroup);
+        					
+        					friendGroup = groupList.get(i);
+        					
+        					groupIndex = i;
+    					}
+    					else {
+    						groupList.get(i).addAll(friendGroup);
+    						
+    						friendGroup = groupList.get(i);
+    						
+    						if (groupList.size() > 1) {
+    							groupList.remove(groupIndex);
+    						}
+    						
+    						groupIndex = i;
+    					}
+    				}
+    			}
+    		}
+    		
+    		if (isNewGroup) {
+    			groupList.add(friendGroup);
+    		}
     	}
         
-    	return 0;
+    	return groupList.size();
     }
 }
--- FriendCircles/src/Tester.java
+++ FriendCircles/src/Tester.java
@@ -3,15 +3,18 @@ public class Tester {
 
 	public static void main(String[] args) {
 		int[][] test = {
-				{1, 1, 0},
-				{1, 1, 0},
-				{0, 0, 1}
+				{1, 0, 0, 1},
+				{0, 1, 1, 0},
+				{0, 1, 1, 1},
+				{1, 0, 1, 1}
 		};
 		
 		
 		FriendCircle ob = new FriendCircle();
 		
 		int result = ob.findCircleNum(test);
+		
+		System.out.println("result: " + result);
 	}
 
 }
