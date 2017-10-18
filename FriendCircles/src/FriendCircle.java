8d5ee99812c8f9f615cc8e27b5ab45c1c497a009
Lam Nguyen <lqn2257@truman.edu>
2017-10-18 14:16:38 -0500

    clean up

--- FriendCircles/src/FriendCircle.java
+++ FriendCircles/src/FriendCircle.java
@@ -14,71 +14,29 @@ import java.util.Set;
 public class FriendCircle {
     public int findCircleNum(int[][] M) {
     	ArrayList<Set> groupList = new ArrayList<Set>();
-//    	int col = 0;
     	
     	for (int friend = 0; friend < M.length; friend++) {
     		Set<Integer> friendGroup = new HashSet<Integer>();
     		boolean isNewGroup = true;
-    		int groupIndex = -1;
-    		
-//    		for (int i = 0; i < groupList.size(); i++) {
-//    			if (groupList.get(i).contains(friend)) {
-//    				friendGroup = groupList.get(i); //Check if the friend already belongs to a group
-//    				
-//    				isNewGroup = false;
-//    				
-//    				groupIndex = i;
-//    			}
-//    		}
+    		int groupIndexToRemove = -1;
     		
     		friendGroup.add(friend);
     		
     		for (int col = friend - 1; col >= 0; col--) { //Scan to the left
-//    			System.out.print(M[friend][col] + " ");
     			if (M[friend][col] == 1) {
     				friendGroup.add(col);
-    				
-//    				for (int i = 0; i < groupList.size(); i++) {
-//    					if (groupList.get(i).contains(col)) {
-//    						isNewGroup = false;
-//    						
-//    						for (Integer newFriend : friendGroup) {
-//    							groupList.get(i).add(newFriend);
-//    						}
-//    						
-//    						friendGroup = groupList.get(i);
-//    						
-//    						groupList.remove(groupIndex);
-//    					}
-//    				}
     			}
     		}
-//    		System.out.print(" + ");
+
     		for (int row = friend + 1; row < M.length; row++) { //Scan below
-//    			System.out.print(M[row][friend] + " ");
     			if (M[row][friend] == 1) {
     				friendGroup.add(row);
-    				
-//    				for (int i = 0; i < groupList.size(); i++) {
-//    					if (groupList.get(i).contains(row)) {
-//    						isNewGroup = false;
-//    						
-//    						for (Integer newFriend : friendGroup) {
-//    							groupList.get(i).add(newFriend);
-//    						}
-//    						
-//    						friendGroup = groupList.get(i);
-//    						
-//    						groupList.remove(groupIndex);
-//    					}
-//    				}
     			}
     		}
-//    		System.out.print("\n");
     		
     		for (Integer newFriend : friendGroup) {
     			for (int i = 0; i < groupList.size(); i++) {
-    				if (i != groupIndex) {
+    				if (i != groupIndexToRemove) {
     					if (groupList.get(i).contains(newFriend)) {
         					if (isNewGroup == true) {
             					isNewGroup = false;
@@ -87,20 +45,20 @@ public class FriendCircle {
             					
             					friendGroup = groupList.get(i);
             					
-            					groupIndex = i;
+            					groupIndexToRemove = i;
         					}
         					else {
         						groupList.get(i).addAll(friendGroup);
         						
         						friendGroup = groupList.get(i);
         						
-        						groupList.remove(groupIndex);
+        						groupList.remove(groupIndexToRemove);
         						
-        						if (i > groupIndex) {
-        							groupIndex = i - 1;
+        						if (i > groupIndexToRemove) {
+        							groupIndexToRemove = i - 1;
         						}
         						else {
-        							groupIndex = i;
+        							groupIndexToRemove = i;
         						}
         					}
         				}
