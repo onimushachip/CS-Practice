c5de6a298c81c004358c62462e44e9bb9d8f1d08
Lam Nguyen <lqn2257@truman.edu>
2017-12-12 16:55:26 -0600

    add Combination Sum 3 prob

---
+++ CombinationSum3/src/CombinationSum.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/*
+ * Find all possible combinations of k numbers that add up to a number n, 
+ * given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+ * 
+ */
+public class CombinationSum {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+    	
+        return null;
+    }
+
+}
