57f9d6b9be03594adf4772713adf726e1d5acec0
Lam Nguyen <lqn2257@truman.edu>
2017-12-12 18:01:26 -0600

    progress

--- CombinationSum3/src/CombinationSum.java
+++ CombinationSum3/src/CombinationSum.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -5,10 +7,41 @@ import java.util.List;
  * given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
  * 
  */
+
 public class CombinationSum {
+	private ArrayList<Integer> combination = new ArrayList<Integer>();
+	
     public List<List<Integer>> combinationSum3(int k, int n) {
+    	findValidComb(1, k, n);
     	
         return null;
+    }
+    
+    private void findValidComb(int nextInt, int layer, int totalSum) {
+    	int sum = 0;
+    	
+    	for (int i = 0; i < this.combination.size(); i++) {
+    		sum += this.combination.get(i);
+    	}
+    	
+    	if (sum > totalSum) {
+    		return;
+    	}
+    	
+    	if (layer == 0) {
+    		if (sum == totalSum) {
+    			System.out.println(Arrays.asList(combination));
+    		}
+    	}
+    	
+    	for (int i = nextInt; i <= 9; i++) {
+    		this.combination.add(i);
+    		
+    		findValidComb(i + 1, layer - 1, totalSum);
+    		
+    		this.combination.remove(combination.size() - 1);
+    	}
+    	
     }
 
 }
---
+++ CombinationSum3/src/Tester.java
@@ -0,0 +1,10 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		CombinationSum ob = new CombinationSum();
+		
+		ob.combinationSum3(3, 9);
+	}
+
+}
